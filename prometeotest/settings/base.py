f67ef7a894bbb1ad6700d2777090dc73bf8c700c
Agustin Machiavello <agmachiavello@gmail.com>
2021-02-26 07:55:11 -0300

    change base confugartion for static files

--- prometeotest/settings/base.py
+++ prometeotest/settings/base.py
@@ -73,6 +73,7 @@ LOCAL_APPS = [
 INSTALLED_APPS = LOCAL_APPS + DJANGO_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -162,3 +163,10 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, '../staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
