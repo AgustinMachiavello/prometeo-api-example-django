e583093bc8890c699a4053b04976a72c083dc5d7
Agustin Machiavello <agmachiavello@gmail.com>
2021-02-26 01:08:01 -0300

    First approach to user account page

--- prometeotest/apps/prometeo/views/account.py
+++ prometeotest/apps/prometeo/views/account.py
@@ -3,11 +3,14 @@ Account page
 """
 
 # Django
-from prometeotest.apps.api.views.api import GetAccount
+from prometeotest.apps.api.views.api import GetAccount, GetUser
 from django.http.response import HttpResponse
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect, reverse
 
+
+# Helpers
+from ..helpers.api import is_request_success, get_request_status, get_request_status_message
 
 class AccountTemplateView(TemplateView):
     """
@@ -20,6 +23,16 @@ class AccountTemplateView(TemplateView):
     context = {}
 
     def get(self, request):
-        self.context['data'] =  GetAccount.as_view()(request).data
+        request_errors = []
+        user_request =  GetUser.as_view()(request).data
+        account_request =  GetAccount.as_view()(request).data
+        if is_request_success(user_request):
+            self.context['user_info'] =  user_request['info']
+        else:
+            self.context['request_errors'] = request_errors.append(get_request_status_message(user_request))
+        if is_request_success(account_request):
+            self.context['accounts'] =  account_request['accounts']
+        else:
+            self.context['request_errors'] = request_errors.append(get_request_status_message(account_request))
         return render(request, self.template_name, self.context)
     
